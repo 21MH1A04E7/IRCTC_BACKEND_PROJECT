a0f15e08294b95f8d778dfe7799ae1ae0b0d91c1
21MH1A04E7 <hk524569@gmail.com>
2026-06-18 22:26:08 +0530

    feat: added joi package

--- backend/admin_service/src/services/station_service.js
+++ backend/admin_service/src/services/station_service.js
@@ -3,13 +3,14 @@ const { ConflictError } = require('../utils/error');
 const Station = require('../models/station')
 
 const createStation = async (data) => {
-    const existing = await Station.query().select('*').where("code", data.code).andWhere("status", 1).first();
+    const existing = await Station.query().select('*').where("code", data.code).first();
 
     if (existing) {
         throw new ConflictError('Station code already exists')
     }
 
-    const station = await Station.query().insert(data);
+    const station = await Station.query().insertAndFetch(data);
+    return station;
 }
 
 
